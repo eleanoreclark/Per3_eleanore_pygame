f47abc05e83c8b73b3320bb32d8020313fb64758
Eleanore Clark <EClark1210@student.pps.net>
2018-05-08 13:32:17 -0700

    crossing lines, lilac background

--- game_window.py
+++ game_window.py
@@ -2,9 +2,16 @@ import pygame
 
 screen = pygame.display.set_mode([640, 480])
 
+
+
 while True:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
 		break
-	screen.fill((255,180,180))
+	
+	screen.fill((255,230,240))
+	pygame.draw.line(screen, (255, 0, 0), (0, 0), (639, 479))
+	pygame.draw.aaline(screen, (100, 100, 200), (639, 0), (0, 479))
+	
+	
 	pygame.display.flip()
