a040dda5d96f7fb89f3dcacc1ded37cdce787b36
Eleanore Clark <EClark1210@student.pps.net>
2018-05-16 12:18:20 -0700

    hypno bar

--- colorbars.py
+++ colorbars.py
@@ -1,17 +1,21 @@
 import pygame
 line_y = 0
-velocity = .1
+velocity = 5
 running = 1
-barheight = 124
+barheight = 500
 color_height = int((barheight/2)+1)
+gradient_intensity = int((255/barheight)*2)
 screen = pygame.display.set_mode((800, 600));
-gradient_intensity = 4
  
 barcolor = []
 for i in range(1, color_height):
-	barcolor.append((i*gradient_intensity, 0, (i*gradient_intensity) ))
+	barcolor.append((i*gradient_intensity, 255-i*gradient_intensity, 182 ))
 for i in range(1, color_height):
-    barcolor.append((255 - i*gradient_intensity, 0, (255 - i*gradient_intensity) ))
+    barcolor.append((i*gradient_intensity, 55, (255 - i*gradient_intensity) ))
+for i in range(1, color_height):
+    barcolor.append((i*gradient_intensity, (255 - i*gradient_intensity), i*gradient_intensity) )
+for i in range(1, color_height):
+    barcolor.append(((255 - i*gradient_intensity), 212, (255 - i*gradient_intensity) ))
 	
  
 while running:
@@ -21,11 +25,11 @@ while running:
 		running = 0
 
 	screen.fill((0, 0, 0))
-	for i in range(0, int(barheight)):
+	for i in range(0, int(barheight*2)):
 		pygame.draw.line(screen, barcolor[i], (0, line_y+i), (799, line_y+i))
 
 	line_y += velocity
-	if line_y + int(barheight) > 599+barheight:
+	if line_y + int(barheight) > 300+barheight:
 		line_y = 0-barheight
 
 	pygame.display.flip()
